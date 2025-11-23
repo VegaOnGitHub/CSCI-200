9b9d79729b74ac14ba172f057e3d8b9b262ec560
Daniel Vega <Daniel13Vega7@gmail.com>
2025-11-22 22:05:07 -0700

    l6a

--- Set6/L6A/Array.hpp
+++ Set6/L6A/Array.hpp
@@ -6,6 +6,7 @@
 #include <exception>
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 template<typename T>
 class Array final : public IList<T> {
@@ -41,7 +42,7 @@ Array<T>::Array() {
 template<typename T>
 Array<T>::~Array() {
     // deallocate current array
-    delete _pArray[];
+    delete[] _pArray;
 }
 
 template<typename T>
@@ -52,111 +53,153 @@ int Array<T>::size() const {
 template<typename T>
 T Array<T>::get(const int POS) const {
     // if POS is out of range, throw std::out_of_range exception
-    if(POS + 1 <= 0 || POS > _size - 1){
-        throw std::out_of_range();
-        std::cerr << "The position " << POS << "is outside the range of the array of size " << _size << std::endl;
+    if (POS < 0 || POS >= _size) {
+        throw std::out_of_range("invalid access at pos " + std::to_string(POS) +
+                                " for size " + std::to_string(_size));
     }
+    return _pArray[POS];
     // return value at POS within array
-    return T();
 }
 
 template<typename T>
 void Array<T>::set(const int POS, const T VALUE) {
     // if POS is out of range, throw std::out_of_range exception
-    if (POS < 0 || POS > _size + 1){
-        throw std::out_of_range();
-        std::cerr << "The position " << POS << "is outside the range of the array of size " << _size << std::endl;
+    if (_size == 0 || POS < 0 || POS >= _size) {
+        throw std::out_of_range("invalid set at pos " + std::to_string(POS) +
+                                " for size " + std::to_string(_size));
     }
     
     // set VALUE at POS within array    
-    else {
-        *_Array[POS - 1] = VALUE;
-        _size++;
-    }
+    _pArray[POS] = VALUE;
 
 }
 
 template<typename T>
 void Array<T>::insert(const int POS, const T VALUE) {
     // if POS is before zero, clamp to zero
-    if (POS < 0){
-        POS = 0;
+    int pos = POS;
+    if (pos < 0){
+        pos = 0;
     }
     // if POS is after size, clamp to size
-    if (POS > _size + 1){
-        POS = _size + 1;
+    if (pos > _size){
+        pos = _size;
     }
 
     // create new array of size + 1
-    T* tempArray[_size];
+    T* tempArray = new T[_size + 1];
     // copy elements 0 to POS from old array to new array
-    for (i = 0; i < POS - 1; i++){
-        *tempArray[i] = *_pArray[i];
+    for (int i = 0; i < pos; i++){
+        tempArray[i] = _pArray[i];
     }
     // set element at POS in new array to VALUE
-    *tempArray[POS] = VALUE;
+    tempArray[pos] = VALUE;
     
     // copy elements POS to size from old array to new array
-    for (i = POS + 1; i > _size; i++){
-        *tempArray[i]; = *_pArray[i];
+    for (int i = pos; i < _size; i++){
+        tempArray[i + 1] = _pArray[i];
     }
     // delete old array
-    
-
+    delete[] _pArray;
     // set old array to be new array
+    _pArray = tempArray;
+
 
     // increment size
+    ++_size;
 
 }
 
 template<typename T>
 void Array<T>::remove(const int POS) {
     // if array is empty, throw std::out_of_range exception
+    if (_size == 0) {
+        throw std::out_of_range("remove on empty array");
+    }
 
     // if POS is before zero, clamp to zero
+    int pos = POS;
+    if (pos < 0) {
+        pos = 0;
+    }
 
     // if POS is after size, clamp to size
+    if (pos >= _size) {
+        pos = _size - 1;
+    }
 
     // create new array of size - 1
+    T* tempArray = (_size > 1) ? new T[_size - 1] : nullptr;
 
     // copy elements from 0 to POS from old array to  new array
+    for (int i = 0; i < pos; ++i) {
+        tempArray[i] = _pArray[i];
+    }
 
     // copy elements from POS+1 to size from old array to new array
+    for (int i = pos + 1; i < _size; ++i) {
+        tempArray[i - 1] = _pArray[i];
+    }
 
     // delete old array
+    delete[] _pArray;
 
     // set old array to be new array
+    _pArray = tempArray;
 
     // decrement size
+    --_size;
 
 }
 
 template<typename T>
 T Array<T>::min() const {
     // if array is empty, throw std::out_of_range exception
+    if (_size == 0) {
+        throw std::out_of_range("min on empty array");
+    }
     
     // find minimum value within array
+    T minVal = _pArray[0];
+    for (int i = 1; i < _size; ++i) {
+        if (_pArray[i] < minVal) {
+            minVal = _pArray[i];
+        }
+    }
     
     // return min value
-    return T();
+    return minVal;
 }
 
 template<typename T>
 T Array<T>::max() const {
     // if array is empty, throw std::out_of_range exception
+    if (_size == 0) {
+        throw std::out_of_range("max on empty array");
+    }
     
     // find maximum value within array
+    T maxVal = _pArray[0];
+    for (int i = 1; i < _size; ++i) {
+        if (_pArray[i] > maxVal) {
+            maxVal = _pArray[i];
+        }
+    }
     
     // return max value
-    return T();
+    return maxVal;
 }
 
 template<typename T>
 int Array<T>::find(const T VALUE) const {
     // search for first occurrence of VALUE
     // hint: start at the beginning and go forward
-    
-    // return index of first occurrence if found
+    for (int i = 0; i < _size; ++i) {
+        if (_pArray[i] == VALUE) {
+            // return index of first occurrence if found
+            return i;
+        }
+    }
     
     // otherwise return -1
     return -1;
@@ -166,8 +209,12 @@ template<typename T>
 int Array<T>::rfind(const T VALUE) const {
     // search for last occurrence of VALUE
     // hint: start at the end and go backward
-
-    // return index of last occurrence if found
+    for (int i = _size - 1; i >= 0; --i) {
+        if (_pArray[i] == VALUE) {
+            // return index of last occurrence if found
+            return i;
+        }
+    }
 
     // otherwise return -1
     return -1;
